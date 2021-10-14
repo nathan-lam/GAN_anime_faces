e75de5072c27fd6557330dc5e70a727361ccf683
nathan-lam <74233738+nathan-lam@users.noreply.github.com>
2021-10-13 21:36:35 -0700

    Delete make_gif.py

--- make_gif.py
+++
@@ -1,8 +0,0 @@
-import imageio
-import os
-
-path = "Attempt3/checkpoint_images/"
-file_names = os.listdir(path)
-
-images = [imageio.imread(path+file) for file in file_names]
-imageio.mimsave('image_epoch.gif', images)
