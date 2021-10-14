749989a0d6aa02c4f9a7d7152bc185976d6e5afb
nathan-lam <74233738+nathan-lam@users.noreply.github.com>
2021-10-13 22:40:38 -0700

    Add files via upload

---
+++ GAN.py
@@ -0,0 +1,74 @@
+from data_prep import *
+from tensorflow.keras import layers
+
+
+def make_generator_model():
+    model = tf.keras.Sequential()  # layer object
+    model.add(layers.Dense(4 * 4 * 256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())  # does a form of normalizing
+    model.add(layers.LeakyReLU())
+    L = 1
+
+    model.add(layers.Reshape((4, 4, 256)))  # making the layer 3D
+    #print(f"Layer {L} shape: {model.output_shape}")
+    assert model.output_shape == (None, 4, 4, 256)  # Note: None is the batch size
+
+    # opposite of a convolution
+    L += 1
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    #print(f"Layer {L} shape: {model.output_shape}")
+    # assert model.output_shape == (None, 4, 4, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    L += 1
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    #print(f"Layer {L} shape: {model.output_shape}")
+    # assert model.output_shape == (None, 8, 8, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    L += 1
+    model.add(layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    #print(f"Layer {L} shape: {model.output_shape}")
+    # assert model.output_shape == (None, 8, 8, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    L += 1
+    model.add(layers.Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    #print(f"Layer {L} shape: {model.output_shape}")
+    # assert model.output_shape == (None, 64, 64, 3)
+
+    return model
+
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                            input_shape=[64, 64, 3]))  # input dimensions of 1 image
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
---
+++ make_dataset.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+import cv2
+import time
+import matplotlib.pyplot as plt
+
+file_name = "anime_faces/data"
+folder_path = f"C:/Users/nthnt/PycharmProjects/ML_projects/{file_name}/"
+
+names_list = os.listdir(folder_path)
+n = len(names_list)
+print(f"Importing {n} images")
+
+# Looking at a single image
+img = cv2.imread(folder_path + names_list[0])
+img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+"""
+plt.imshow(img_cvt)
+plt.axis('off')
+plt.show()
+"""
+
+# building the dataset
+start = time.time()
+data_set = np.copy(img.reshape(1, img.shape[0], img.shape[1], img.shape[2]))
+for i in range(1, len(names_list)):
+    if i % 1000 == 0:
+        print(f"{i}/{n}")
+    img = cv2.imread(folder_path + names_list[i])  # read in image
+    img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # correct the color values
+    data_set = np.concatenate((data_set, img_cvt.reshape(1, img.shape[0], img.shape[1], img.shape[2])), axis=0)
+print(f"Process took {time.time() - start} seconds")
+np.save("C:/Users/nthnt/PycharmProjects/ML_projects/anime_faces.npy", data_set)
