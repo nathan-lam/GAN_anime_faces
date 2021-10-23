b32b73c871fc927e255acee0096ee9f08a3af27a
nathan-lam <74233738+nathan-lam@users.noreply.github.com>
2021-10-23 15:25:51 -0700

    adjusting to new folder layout

--- make_dataset.py
+++ make_dataset.py
@@ -4,8 +4,8 @@ import cv2
 import time
 import matplotlib.pyplot as plt
 
-file_name = "anime_faces/data"
-folder_path = f"C:/Users/nthnt/PycharmProjects/ML_projects/{file_name}/"
+file_name = "anime_faces"
+folder_path = f"C:/Users/nthnt/PycharmProjects/GAN_faces_pycharm/Datasets/{file_name}/"
 
 names_list = os.listdir(folder_path)
 n = len(names_list)
@@ -23,9 +23,10 @@ plt.show()
 # building the dataset
 start = time.time()
 data_set = np.copy(img.reshape(1, img.shape[0], img.shape[1], img.shape[2]))
-for i in range(1, len(names_list)):
-    if i % 1000 == 0:
-        print(f"{i}/{n}")
+n = len(names_list)
+for i in range(1, n):
+    if i % (n//100) == 0:
+        print(f"{i}/{n} = {int(i/n*100)}%")
     img = cv2.imread(folder_path + names_list[i])  # read in image
     img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # correct the color values
     data_set = np.concatenate((data_set, img_cvt.reshape(1, img.shape[0], img.shape[1], img.shape[2])), axis=0)
