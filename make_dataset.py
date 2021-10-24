0f01398227a8492a7fa0d7dd3193cc617b6916f9
nathan-lam <74233738+nathan-lam@users.noreply.github.com>
2021-10-24 12:02:47 -0700

    make dataset in parts?
    I want to test if I can make a 20gb dataset in chunks

--- make_dataset.py
+++ make_dataset.py
@@ -1,13 +1,13 @@
 import numpy as np
-import os
-import cv2
-import time
-import matplotlib.pyplot as plt
+import os # get file names from path
+import cv2 # image processing
+import time # Track runtime
 
-file_name = "anime_faces"
-folder_path = f"C:/Users/nthnt/PycharmProjects/GAN_faces_pycharm/Datasets/{file_name}/"
 
-names_list = os.listdir(folder_path)
+file_name = "Human_faces"
+folder_path = f"C:/Users/nthnt/PycharmProjects/GAN_faces_pycharm/Datasets/"
+
+names_list = os.listdir(folder_path+file_name+"/")
 n = len(names_list)
 print(f"Importing {n} images")
 
@@ -22,13 +22,20 @@ plt.show()
 
 # building the dataset
 start = time.time()
-data_set = np.copy(img.reshape(1, img.shape[0], img.shape[1], img.shape[2]))
-n = len(names_list)
-for i in range(1, n):
+
+target_path = os.listdir(folder_path)
+if f"np_{file_name}.npy" not in target_path:
+    data_set = np.copy(img.reshape(1, img.shape[0], img.shape[1], img.shape[2]))
+else:
+    data_set = np.load(folder_path+f"np_{file_name}.npy")
+
+n = len(names_list) - len(data_set) + 1
+for i in range(1, 521):
     if i % (n//100) == 0:
         print(f"{i}/{n} = {int(i/n*100)}%")
     img = cv2.imread(folder_path + names_list[i])  # read in image
     img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # correct the color values
     data_set = np.concatenate((data_set, img_cvt.reshape(1, img.shape[0], img.shape[1], img.shape[2])), axis=0)
 print(f"Process took {time.time() - start} seconds")
-np.save("C:/Users/nthnt/PycharmProjects/ML_projects/anime_faces.npy", data_set)
+np.save(folder_path+f"np_{file_name}.npy", data_set)
+
